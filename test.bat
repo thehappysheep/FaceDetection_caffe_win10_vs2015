d1edd5a239d03b52e3a86cc4b17047c18f6259a5
sunynag <1327623813@qq.com>
2018-08-01 11:00:34 +0800

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,2 @@
+F:/caffe-windows/scripts/build/tools/Release/caffe.exe test --model=E:/dataset/catdog/train_test_net.prototxt  -weights E:/dataset/catdog/model/_iter_10000.caffemodel -iterations 472
+
